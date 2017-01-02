1d87143df2e55b30a9f37923908094f31b6c95ff
dds <123207246@qq.com>
2017-01-03 01:03:58 +0800

    add alll

---
+++ client/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yangbo on 17/1/2.
+ */
+import React from 'react'
+import {render} from 'react-dom'
+
+
+
+render(
+    <divs>
+       sssname22eee
+    </divs>,
+    document.getElementById('root')
+);
+
